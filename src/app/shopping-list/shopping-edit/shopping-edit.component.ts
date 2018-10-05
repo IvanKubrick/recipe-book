d74278b73c1105e1be8c5898dd76e8f82df59b1a
Ivan Grushevich <githubmail911@gmail.com>
2018-10-05 18:32:35 +0300

    implement template form in shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-shopping-edit',
@@ -9,9 +10,6 @@ import { ShoppingListService } from '../shopping-list.service';
     styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-    @ViewChild('nameInput') nameInputRef: ElementRef;
-    @ViewChild('amountInput') amountInputRef: ElementRef;
-
     constructor(
         private shoppinglistService: ShoppingListService
     ) {}
@@ -19,12 +17,9 @@ export class ShoppingEditComponent implements OnInit {
     ngOnInit() {
     }
 
-    onAddItem() {
-        const ingredientName = this.nameInputRef.nativeElement.value;
-        const ingredientAmount = this.amountInputRef.nativeElement.value;
-        if(!ingredientName || !ingredientAmount) return;
-
-        const newIngredient = new Ingredient(ingredientName, ingredientAmount);
+    onAddItem(form: NgForm) {
+        const value = form.value;
+        const newIngredient = new Ingredient(value.name, value.amount);
         this.shoppinglistService.addIngredient(newIngredient);
     }
 }
