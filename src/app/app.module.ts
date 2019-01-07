a60593a23aa25f3dc5acc1772b63b206f773e713
Ivan Grushevich <githubmail911@gmail.com>
2019-01-08 02:33:25 +0300

    Implement basic effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { CoreModule } from './core/core.module';
 import { reducers } from './store/app.reducers';
+import { AuthEffects } from './auth/store/auth.effects';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,7 +23,8 @@ import { reducers } from './store/app.reducers';
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    StoreModule.forRoot(reducers)
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AuthEffects])
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/auth/store/auth.effects.ts
@@ -0,0 +1,39 @@
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
+import * as firebase from 'firebase';
+import { from } from 'rxjs/observable/from';
+
+import * as AuthActions from './auth.actions';
+
+@Injectable()
+export class AuthEffects {
+  @Effect()
+  authSignup = this.actions$.pipe(
+    ofType(AuthActions.TRY_SIGNUP),
+    map((action: AuthActions.TrySignup) => action.payload),
+    switchMap((authData: { username: string; password: string }) => {
+      return from(
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(authData.username, authData.password)
+      );
+    }),
+    switchMap(() => {
+      return from(firebase.auth().currentUser.getIdToken());
+    }),
+    mergeMap((token: string) => {
+      return [
+        {
+          type: AuthActions.SIGNUP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+      ];
+    })
+  );
+
+  constructor(private actions$: Actions) {}
+}
